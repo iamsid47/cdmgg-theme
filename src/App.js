579fb2ee3362e1f1439a3d7d11d4e3d737a6e0b8
iamsid47 <siddheshkulthe43@gmail.com>
2021-10-06 23:02:38 +0530

    minpr changes

--- src/App.js
+++ src/App.js
@@ -39,9 +39,9 @@ function App() {
         <div className="bg-gray-50">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              <span className="block">Ready to dive in?</span>
+              <span className="block">Join The Community</span>
               <span className="block text-pr-offRed">
-                Start your free trial today.
+                Learn. Develop. Iterate.
               </span>
             </h2>
             <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
